0ddeec5f2a6d7c04e03c68174a566b0eaec04f7f
Vrush2004 <vrushalichaube@gmail.com>
2023-07-15 15:44:45 +0530

    highlight active link

--- frontend/src/components/General/BottomNav.jsx
+++ frontend/src/components/General/BottomNav.jsx
@@ -1,28 +1,46 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate, useLocation } from 'react-router-dom'
 
 const BottomNav = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const commonClasses = "text-sm text-gray-600 dark:text-gray-400";
+
     return (
         <div className="fixed bottom-0 left-0 z-50 w-full h-14 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
             <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium ">
-                <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                    <svg className="w-6 h-6 mb-1 text-gray-600 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-orange-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                <button
+                    type="button"
+                    to="/"
+                    className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group tahir"
+                    onClick={() => navigate('/')}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={location.pathname === '/' ? "#ff8000" : "currentColor"} className="w-6 h-6 mb-1 text-gray-600 dark:text-gray-400">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                     </svg>
-                    <NavLink to="/" value="/" className="text-sm text-gray-600 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-orange-500">Home</NavLink>
+
+                    <span className={location.pathname === '/' ? `${commonClasses} tahir` : `${commonClasses}`}>Home</span>
                 </button>
-                <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mb-1 text-gray-600 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-orange-500">
+
+                <button type="button"
+                    className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+                    onClick={() => navigate('/products')}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={location.pathname === '/products' ? "#ff8000" : "currentColor"} className="w-6 h-6 mb-1 text-gray-600 dark:text-gray-400">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
                     </svg>
-                    <NavLink to="/products" value="/products" className="text-sm text-gray-600 dark:text-gray-400 group-hover:text-orange-600 group-hover:font-bold dark:group-hover:text-orange-500">Products</NavLink>
+                    <NavLink to="/products" value="/products" className={location.pathname === '/products' ? `${commonClasses} tahir` : `${commonClasses}`}>Products</NavLink>
                 </button>
-                <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mb-1 text-gray-600 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-orange-500">
+                <button type="button"
+                    className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+                    onClick={() => navigate('/orders')}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke={location.pathname === '/orders' ? "#ff8000" : "currentColor"} className="w-6 h-6 mb-1 text-gray-600">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                     </svg>
-                    <span to="/orders" value="/orders" className="text-sm text-gray-600 dark:text-gray-400 group-hover:text-orange-600 group-hover:font-bold dark:group-hover:text-orange-600">Orders</span>
+                    <span to="/orders" value="/orders" className={location.pathname==='/orders'?`${commonClasses} tahir`: `${commonClasses}`}>Orders</span>
                 </button>
 
             </div>
